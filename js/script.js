160da4e8a05695c384c7781ab13eebfe83b8c20e
Feer Aleksandr <pertik@list.ru>
2017-11-04 15:24:18 +0300

    add menu, team and map section

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	// $('.team__link').on('click', function(e) {
+	// 	e.preventDefault();
+
+	// 	let $this = $(this),
+	// 		item = $this.closest('.team__item'),
+	// 		list = $this.closest('.team__list'),
+	// 		items = list.find('.team__item'),
+	// 		content = item.find('.team__inner'),
+	// 		otherContent = list.find('.team__inner'),
+	// 		duration = 300;
+
+	// 	if(!item.hasClass('team__item-active')) {
+	// 		items.removeClass('team__item-active');
+	// 		item.addClass('team__item-active');
+
+	// 		otherContent.stop(true, true).slideUp(duration);
+	// 		content.stop(true, true).slideDown(duration);
+	// 	} else {
+	// 		content.stop(true, true).slideDown(duration);
+	// 		item.stop(true, true).removeClass('team__item-active');
+	// 	}
+	// });
+});
