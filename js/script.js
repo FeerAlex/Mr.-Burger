5031f0b00d7f634494d5fdd385f73ea8c89ad5d4
Feer Aleksandr <pertik@list.ru>
2017-11-13 00:48:07 +0300

    add one-page-scroll

--- js/script.js
+++ js/script.js
@@ -1,6 +1,77 @@
+let _onePageScroll = (function()  {
+	let container	= $('.main'),
+		points		= $('.points__link'),
+		sections	= $('.section'),
+		screen		= 0,
+		inScroll	= false;
+
+	$('.section:first-child').addClass('section--active');
+
+	let _scroll = function(e) {
+		let activePage = sections.filter('.section--active');
+
+		if(!inScroll) {
+			inScroll = true;
+
+			if (e.deltaY > 0) {
+				if(activePage.prev().length) {
+					screen--;
+				}
+			} else {
+				if(activePage.next().length) {
+					screen++;
+				}
+			}
+		}
+
+		let pos		= (-screen * 100) + '%';
+		sections.eq(screen).addClass('section--active').siblings().removeClass('section--active');
+
+		container.css('top', pos);
+
+		setTimeout(function() {
+			inScroll = false;
+		}, 1300);
+	};
+
+	let _scrollClick = function(e) {
+		points.removeClass('points__link--active');
+		let point = points.filter(`[href^='${e.target.hash}']`);
+		point.addClass('points__link--active');
+
+		container.stop().animate({
+			top: - $(e.target.hash).position().top
+		}, 1000);
+	}
+	
+	return {
+		init: function() {
+			$('body').on('mousewheel', function(e) {
+				_scroll(e);
+			});
+
+			$('.nav__link, .points__link, .arrow__link').on('click', function(e) {
+				e.preventDefault();
+				_scrollClick(e);
+			});
+		}
+	}
+}());
+
+let _menuClick = (function()  {
+	return {
+		init: function() {
+			$('#gamburger').click(function(){
+				$(this).toggleClass('btn-menu--open');
+				$('.header__nav').toggleClass('header__nav--active');
+			});
+		}
+	}
+}());
+
 $(document).ready(function(){
-	$('#gamburger').click(function(){
-		$(this).toggleClass('btn-menu--open');
-		$('.header__nav').toggleClass('header__nav--active');
-	});
+	
+	_onePageScroll.init();
+
+	_menuClick.init();
 });
