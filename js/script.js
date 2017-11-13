ad67395571264031b0545d35ad52d5b4cb8f591b
Feer Aleksandr <pertik@list.ru>
2017-11-13 09:14:40 +0300

    edit animation for menu

--- js/script.js
+++ js/script.js
@@ -63,7 +63,7 @@ let _menuClick = (function()  {
 		init: function() {
 			$('#gamburger').click(function(){
 				$(this).toggleClass('btn-menu--open');
-				$('.header__nav').toggleClass('header__nav--active');
+				$('.nav').toggleClass('nav--active');
 			});
 		}
 	}
